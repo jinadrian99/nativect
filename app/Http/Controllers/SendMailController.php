edd7d5e4a0dc73fcf895d8d04c30d4e332d127ab
hanquocadrian <hanquocadrian@gmail.com>
2021-03-06 20:05:26 +0700

    fix send_mail deploy v3

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -37,132 +37,133 @@ class SendMailController extends Controller
     }
     public function sendMail(Request $request)
     {
-        // Load Composer's autoloader
-        // require 'vendor/autoload.php';
+        return $request->tenKH;
+        // // Load Composer's autoloader
+        // // require 'vendor/autoload.php';
 
-        // Instantiation and passing `true` enables exceptions
-        $mail = new PHPMailer(true);
+        // // Instantiation and passing `true` enables exceptions
+        // $mail = new PHPMailer(true);
 
-        try {
-            $mail->SMTPDebug = 0;                     
-            $mail->isSMTP();                                           
-            $mail->Host       = 'smtp.gmail.com';                    
-            $mail->SMTPAuth   = true;                                   
-            $mail->Username   = 'nativehotelct@gmail.com';                    
-            $mail->Password   = 'Nh@123456';                              
-            $mail->SMTPSecure = 'tls';    
-            $mail->Port       = 587; 
+        // try {
+        //     $mail->SMTPDebug = 0;                     
+        //     $mail->isSMTP();                                           
+        //     $mail->Host       = 'smtp.gmail.com';                    
+        //     $mail->SMTPAuth   = true;                                   
+        //     $mail->Username   = 'nativehotelct@gmail.com';                    
+        //     $mail->Password   = 'Nh@123456';                              
+        //     $mail->SMTPSecure = 'tls';    
+        //     $mail->Port       = 587; 
 
-            $mail->setFrom('nativehotelct@gmail.com', 'Native Hotel');
-            $mail->addAddress($request->email, $request->tenKH);    
+        //     $mail->setFrom('nativehotelct@gmail.com', 'Native Hotel');
+        //     $mail->addAddress($request->email, $request->tenKH);    
 
-            $mail->AddEmbeddedImage('image/NativeAva.jpg', 'native_ava');
+        //     $mail->AddEmbeddedImage('image/NativeAva.jpg', 'native_ava');
 
-            $textBody='
-                <head>
-                    <meta charset="UTF-8" />
-                </head>
-                <body style="font-family: Arial, Helvetica, sans-serif; color: black">
-                    <table style="margin: 0 auto;">
-                        <tr>
-                            <td width="320">
-                                <img
-                                width="42"
-                                height="42"
-                                alt="Native"
-                                border="0"
-                                style="border: none; padding: 0; margin: 0;"
-                                src="cid:native_ava"
-                                />
-                            </td>
-                            <td>
-                                <h1 style="margin-top: 20px; text-align:right">Biên Nhận</h3>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"><hr></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Name: '. $request->tenKH .'
-                            </td>
-                            <td>
-                                ID Booking: '. $request->idBooking .'
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Email: '. $request->email .'
-                            </td>
-                            <td>
-                                Amount room: '. $request->slPhong .'
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Phone: '. $request->sdt .'
-                            </td>
-                            <td>
-                                Room: '. $request->tenLP .'
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Bank: '. (($request->loaiThe==0) ? 'VISA' : (($request->loaiThe==1) ? 'MasterCard' : (($request->loaiThe==2) ? 'Domestic' : ''))) .'_'. (($request->nganHang==0) ? 'BIDV' : (($request->nganHang==1) ? 'Eximbank' : '')) .'
-                            </td>
-                            <td>
-                                Arrive: '. $request->ngayDen .'
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                ID CARD: '. $request->soThe .'
-                            </td>
-                            <td>
-                                Depart: '. $request->ngayDi .'
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"><hr></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2" style="text-align: center; padding-top: 20px">
-                                <i>Date Booking: '. $request->ngayDat .'</i>
-                                <h3>
-                                    Total Price: <span>'. $request->tongTien .' VND</span>
-                                </h3> 
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2" style="text-align: center;">
-                                <img
-                                    width="24"
-                                    height="24"
-                                    alt="Native"
-                                    border="0"
-                                    style="border: none; padding: 0; margin: 0;"
-                                    src="cid:native_ava"
-                                />
-                                <br>
-                                <a href="http://nativect.herokuapp.com/" target="_blank">
-                                    Go to Native
-                                </a>
-                            </td>
-                        </tr>
-                    </table>
-                </body>
-            ';
+        //     $textBody='
+        //         <head>
+        //             <meta charset="UTF-8" />
+        //         </head>
+        //         <body style="font-family: Arial, Helvetica, sans-serif; color: black">
+        //             <table style="margin: 0 auto;">
+        //                 <tr>
+        //                     <td width="320">
+        //                         <img
+        //                         width="42"
+        //                         height="42"
+        //                         alt="Native"
+        //                         border="0"
+        //                         style="border: none; padding: 0; margin: 0;"
+        //                         src="cid:native_ava"
+        //                         />
+        //                     </td>
+        //                     <td>
+        //                         <h1 style="margin-top: 20px; text-align:right">Biên Nhận</h3>
+        //                     </td>
+        //                 </tr>
+        //                 <tr>
+        //                     <td colspan="2"><hr></td>
+        //                 </tr>
+        //                 <tr>
+        //                     <td>
+        //                         Name: '. $request->tenKH .'
+        //                     </td>
+        //                     <td>
+        //                         ID Booking: '. $request->idBooking .'
+        //                     </td>
+        //                 </tr>
+        //                 <tr>
+        //                     <td>
+        //                         Email: '. $request->email .'
+        //                     </td>
+        //                     <td>
+        //                         Amount room: '. $request->slPhong .'
+        //                     </td>
+        //                 </tr>
+        //                 <tr>
+        //                     <td>
+        //                         Phone: '. $request->sdt .'
+        //                     </td>
+        //                     <td>
+        //                         Room: '. $request->tenLP .'
+        //                     </td>
+        //                 </tr>
+        //                 <tr>
+        //                     <td>
+        //                         Bank: '. (($request->loaiThe==0) ? 'VISA' : (($request->loaiThe==1) ? 'MasterCard' : (($request->loaiThe==2) ? 'Domestic' : ''))) .'_'. (($request->nganHang==0) ? 'BIDV' : (($request->nganHang==1) ? 'Eximbank' : '')) .'
+        //                     </td>
+        //                     <td>
+        //                         Arrive: '. $request->ngayDen .'
+        //                     </td>
+        //                 </tr>
+        //                 <tr>
+        //                     <td>
+        //                         ID CARD: '. $request->soThe .'
+        //                     </td>
+        //                     <td>
+        //                         Depart: '. $request->ngayDi .'
+        //                     </td>
+        //                 </tr>
+        //                 <tr>
+        //                     <td colspan="2"><hr></td>
+        //                 </tr>
+        //                 <tr>
+        //                     <td colspan="2" style="text-align: center; padding-top: 20px">
+        //                         <i>Date Booking: '. $request->ngayDat .'</i>
+        //                         <h3>
+        //                             Total Price: <span>'. $request->tongTien .' VND</span>
+        //                         </h3> 
+        //                     </td>
+        //                 </tr>
+        //                 <tr>
+        //                     <td colspan="2" style="text-align: center;">
+        //                         <img
+        //                             width="24"
+        //                             height="24"
+        //                             alt="Native"
+        //                             border="0"
+        //                             style="border: none; padding: 0; margin: 0;"
+        //                             src="cid:native_ava"
+        //                         />
+        //                         <br>
+        //                         <a href="http://nativect.herokuapp.com/" target="_blank">
+        //                             Go to Native
+        //                         </a>
+        //                     </td>
+        //                 </tr>
+        //             </table>
+        //         </body>
+        //     ';
 
-            $mail->isHTML(true);  
-            $mail->CharSet = "UTF-8";                
-            $mail->Subject = 'Biên nhận của bạn từ Native';
-            $mail->Body    = $textBody;
+        //     $mail->isHTML(true);  
+        //     $mail->CharSet = "UTF-8";                
+        //     $mail->Subject = 'Biên nhận của bạn từ Native';
+        //     $mail->Body    = $textBody;
 
-            $mail->send();
-            return 'true';
-        } catch (Exception $e) {
-            // return "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-            return 'false';
-        }
+        //     $mail->send();
+        //     return 'true';
+        // } catch (Exception $e) {
+        //     // return "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        //     return 'false';
+        // }
     }
 }
